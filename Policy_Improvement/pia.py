433ab0a6a50ad5deaf09901f440de7faf7e733aa
Nischal Aryal <nischal5123.github.io>
2022-11-23 01:10:16 -0800

    Policy Iteration/Improvement complete

--- Policy_Improvement/pia.py
+++ Policy_Improvement/pia.py
@@ -1,47 +1,117 @@
 import numpy as np
 
-def pia(gamma,R,P):
-    """.
+
+def initilize_pi(P):
+    """To initialize pi: for every state s, look up the actions that can be taken there, and assign pi[s,a] = 1/(number of actions available at s)"""
+    nS, _, nA = np.shape(P)
+    pi = np.zeros((nS, nA))  # start with all states have transitions for all actions
+    for s in range(nS):
+        for a in range(nA):
+            for s_prime in range(nS):
+                if (
+                    P[s][s_prime][a]
+                ) != 0:  # if  transition doesn't exist policy set to 0
+                    pi[s][a] = 1
+
+    for i in range(nS):
+        available_actions_s = pi[i].sum(axis=0)
+        for j in range(nA):
+            if pi[i][j] != 0:
+                # assign pi[s,a] = 1/(number of actions available at s)
+                pi[i][j] = 1 / available_actions_s
+
+    return pi
+
+
+def policy_evaluation(P, R, policy, gamma):
+    """Policy Evaluation (prediction): Compute the state Value Function for some or all states given a fixed policy.
+
     gamma: the discount factor, which is a float strictly between 0 and 1.
-    the reward array R: that's a n-by-n-by-m array, where n is the number of states in the MDP and m is the number of unique actions.
-    The transition probability array P: this is also n-by-n-by-m, where P(s,s',a) =  probability of transitioning to s' given that agent took action a in state s.
+    R:the reward array: that's a n-by-n-by-m array, where n is the number of states in the MDP and m is the number of unique actions.
+    P:The transition probability array: this is also n-by-n-by-m, where P(s,s',a) =  probability of transitioning to s' given that agent took action a in state s.
+    policy: p[i,j]
 
+    return V: Value function for policy V[pi]
     """
-    print(np.shape(R))
-    N_STATES, N_ACTIONS, _ = np.shape(R)
+    theta = 0.000001  # to measure significane of the improvement
+    max_iterations = 99999999999
+    nS, _, nA = np.shape(R)  # nS: number of States, nA:number of Actions
+    V = np.zeros(nS)
+
+    # FAIL SAFE: to stop iteration even if the change of value function is never less than theta
+    for i in range(int(max_iterations)):
+        # Initialize a change of value function as zero
+        delta = 0
+        for s in range(nS):
+            # initialize state-action-value
+            v_sa = 0
+            for a in range(nA):
+                for s_prime in range(nS):
+                    v_sa += (
+                        (policy[s][a])
+                        * P[s, s_prime, a]
+                        * (R[s, s_prime, a] + gamma * V[s_prime])
+                    )
+
+            # delta:change in value of state
+            delta = max(delta, np.abs(V[s] - v_sa))
+            V[s] = v_sa
+
+        # Repeat until change in value is below the threshold
+        if delta < theta:
+            return V
+
+
+def pia(gamma, P, R):
+    """
+    gamma: the discount factor, which is a float strictly between 0 and 1.
+
+    The MDP description: consists in two matrices.
+    R:the reward array: that's a n-by-n-by-m array, where n is the number of states in the MDP and m is the number of unique actions.
+    P:The transition probability array: this is also n-by-n-by-m, where P(s,s',a) =  probability of transitioning to s' given that agent took action a in state s.
+
+    print Policy and Value
+    """
+    max_iterations = 99999999999
+    nS, _, nA = np.shape(R)
     # initialize policy and value arbitrarily
-    policy = [0 for s in range(N_STATES)]
-    V = np.zeros(N_STATES)
+    pi = initilize_pi(P)
+    print(f"INITIAL POLICY \n{pi}\n")
+    # FAIL SAFE: to stop iteration even if we never get a stable policy, this can be useful if the updates keep switching between 2 equally good policy
+    for i in range(int(max_iterations)):
+        # Evaluate current policy by computing the valur function
+        V = policy_evaluation(P, R, pi, gamma)
+        stable_policy = True
+        # Go through each state and try to improve actions that were taken (policy Improvement)
+        for s in range(nS):
+            # policy action: best action for current state determined by the policy
+            policy_action = np.argmax(pi[s])
 
-    print("Initial policy", policy)
-    # print V
-    # print P
-    # print R
+            q_sa = np.zeros(nA)
+            for a in range(nA):
+                for s_prime in range(nS):
+                    q_sa[a] += [
+                        P[s, s_prime, a] * (R[s, s_prime, a] + gamma * V[s_prime])
+                    ]
 
-    is_value_changed = True
-    iterations = 0
-    while is_value_changed:
-        is_value_changed = False
-        iterations += 1
-        # run value iteration for each state
-        for s in range(N_STATES):
-            V[s] = sum([P[s, policy[s], s1] * (R[s, policy[s], s1] + gamma * V[s1]) for s1 in range(N_STATES)])
-            # print "Run for state", s
+            # action/s that provided the best value; not based on the policy
+            all_best_value_actions = (
+                (np.argwhere(q_sa == np.max(q_sa))).flatten().tolist()
+            )
 
-        for s in range(N_STATES):
-            q_best = V[s]
-            # print "State", s, "q_best", q_best
-            for a in range(N_ACTIONS):
-                q_sa = sum([P[s, a, s1] * (R[s, a, s1] + gamma * V[s1]) for s1 in range(N_STATES)])
-                if q_sa > q_best:
-                    print("State", s, ": q_sa", q_sa, "q_best", q_best)
-                    policy[s] = a
-                    q_best = q_sa
-                    is_value_changed = True
+            # if 2 or more actions are equally good in terms of action value then policy can choose any
+            updated_action_probability = 1 / len(
+                all_best_value_actions
+            )  # if only one best value action: pick that action with probability 1/1
+            pi[s] = np.zeros(nA)
+            for act in all_best_value_actions:
+                pi[s][act] = updated_action_probability
 
-        print("Iterations:", iterations)
-        # print "Policy now", policy
+            if policy_action not in all_best_value_actions:
+                stable_policy = False  # policy has further room to improve; not stable
 
-    print("Final policy")
-    print(policy)
-    print(V)
+        # When policy is stable: Decide on a termination criterion for the iterations - when does the code stop updating the policy?
+        if stable_policy:
+            print(f"FINAL POLICY:  \n{pi}\n")
+            print("VALUE:  ", V)
+            break
--- Policy_Improvement/tb.py
+++ Policy_Improvement/tb.py
@@ -1,30 +1,89 @@
 import pia
 import numpy as np
+import random
 
 
+class MDP:
 
-def generate_MDP(n_states,n_actions):
-states = [0,1,2,3,4]
-actions = [0,1]
-N_STATES = len(states)
-N_ACTIONS = len(actions)
-P = np.zeros((N_STATES, N_ACTIONS, N_STATES))  # transition probability
-R = np.zeros((N_STATES, N_ACTIONS, N_STATES))  # rewards
+    """
+    The MDP description: consists in two matrices.
+    the reward array R: that's a n-by-n-by-m array, where n is the number of states in the MDP and m is the number of unique actions.
+    R[s,s',a] = the reward for going from s to s' by taking action a.
+    If there is no transition (s,a,s') in the MDP, then the value of R[s,s'a] is zero.
 
-P[0,0,1] = 1.0
-P[1,1,2] = 1.0
-P[2,0,3] = 1.0
-P[3,1,4] = 1.0
-P[4,0,4] = 1.0
+    """
+
+    def __init__(self, n_states, n_actions, min_reward=-100, max_reward=100):
+        self.n_states = n_states
+        self.n_actions = n_actions
+        self.max_reward = max_reward
+        self.min_reward = min_reward
+        self.P = None
+        self.R = None
+
+    def generate_states_actions(self):
+        states = np.arange(0, self.n_states, 1)
+        actions = np.arange(0, self.n_actions, 1)
+
+        return states, actions
+
+    def generate_transitions(self):
+        P = np.random.rand(self.n_states, self.n_states, self.n_actions)
+        self.P = (
+            P / P.sum(axis=1)[:, None]
+        )  # transition probability : probabilities sum to 1
+
+    def generate_rewards(self):
+        self.R = np.random.randint(
+            self.min_reward,
+            high=self.max_reward,
+            size=(self.n_states, self.n_states, self.n_actions),
+        )
+        # If there is no transition (s,a,s') in the MDP, then the value of R[s,s'a] is zero.
+        self.R[self.P == 0] = 0
 
 
-R[0,0,1] = 1
-R[1,1,2] = 10
-R[2,0,3] = 100
-R[3,1,4] = 1000
-R[4,0,4] = 1.0
+def main():
 
-gamma = 0.75
+    # define number of states and actions
+    number_of_states, number_of_actions = 5, 3
+
+    # define distribution of reward  -inf to +inf for test -100 to +100
+    minimum_reward, maximum_reward = -100, 100
+    assert minimum_reward < maximum_reward
+
+    gamma = random.uniform(0, 1)
+
+    print(
+        f"#####################  MDP designed with {number_of_states} States, {number_of_actions} Actions and gamma {gamma} ######################\n"
+    )
+
+    my_mdp = MDP(number_of_states, number_of_actions, minimum_reward, maximum_reward)
+    my_mdp.generate_states_actions()
+    my_mdp.generate_transitions()
+    my_mdp.generate_rewards()
+
+    pia.pia(gamma, my_mdp.P, my_mdp.R)
 
 
-pia.pia(gamma,R,P)
+# #basic test
+#     gamma=0.9
+#     P = np.zeros((number_of_states, number_of_states, number_of_actions))
+#     P[0, 1, 0] = 0.3
+#     P[1, 2, 1] = 0.5
+#     P[2, 3, 0] = 0.5
+#     P[3, 4, 1] = 0.5
+#     P[4, 4, 0] = 0.34
+#     P[0, 1, 2] = 0.4
+#     P[1, 2, 0] = 0.5
+#     P[2, 3, 1] = 0.5
+#     P[3, 4, 0] = 0.5
+#     P[4, 4, 2] = 0.33
+#     P[4, 4, 1] = 0.33
+#     R = np.copy(P)  # rewards
+#     R[R > 0] = 10
+#     pia.pia(gamma, P, R)
+
+
+if __name__ == "__main__":
+    main()
